4815a73ffe05c26598f4a7cff4f7cd32688378bc
Mark Brooks <mdbrooks10@gmail.com>
2022-05-25 09:14:15 +0100

    somewhat working, math needs correcting

--- python/compoundinterestcalc.py
+++ python/compoundinterestcalc.py
@@ -1,14 +1,24 @@
 #calculate how much compound interest you might earn over a set amount of time
 
 def calc():
+    year = 0
     interest_rate = input("What is you interest rate? - %")
     investment = input("How much do you invest? - £")
     time_scale = input("When do you want to withdraw? - ")
+    print(range(1,int(time_scale)))
     info_check = input(f"So you want to invest £{investment} over {time_scale} years with an interest rate of {interest_rate}%? Y/N - ")
-    if info_check == "N":
+    print(info_check)
+    if info_check.upper() == "N":
         calc()
-    elif info_check == "Y":
-        interest = float(investment) / 100 * float(interest_rate)
+    elif info_check.upper() == "Y":
+        
+        while year < int(time_scale):
+            interest = float(investment) / 100 * float(interest_rate) + float(investment)
+            investment = interest
+            year += 1
+            continue
+
+        print(f"£{investment}")
 
     else:
         print("Please answer Y or N...")
